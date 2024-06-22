082f6b761110de918998bceb967ea3e3ab34b233
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-22 15:47:07 +0500

    Register DbContext in DI Container

--- CompanyEmployeeApi/Program.cs
+++ CompanyEmployeeApi/Program.cs
@@ -1,9 +1,21 @@
+using DB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration
+    .GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException(
+        "Не удалось найти строку подключения к базе данных 'DefaultConnection'. Пожалуйста, укажите её в конфигурации вашего приложения.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
