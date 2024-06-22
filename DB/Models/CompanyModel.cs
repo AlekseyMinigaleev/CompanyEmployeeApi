49e61304143d439a9e418a6918e6c61ae4cb63e7
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-22 15:18:16 +0500

    Add project files.

---
+++ DB/Models/CompanyModel.cs
@@ -0,0 +1,66 @@
+﻿namespace DB.Models
+{
+    /// <summary>
+    /// Модель компании.
+    /// </summary>
+    public class CompanyModel(
+        string name,
+        string address,
+        string industry,
+        ICollection<EmployeeModel> employees)
+    {
+        /// <summary>
+        /// Уникальный идентификатор компании.
+        /// </summary>
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Название компании.
+        /// </summary>
+        public string Name { get; private set; } = name;
+
+        /// <summary>
+        /// Адрес компании.
+        /// </summary>
+        public string Address { get; private set; } = address;
+
+        /// <summary>
+        /// Отрасль компании.
+        /// </summary>
+        public string Industry { get; private set; } = industry;
+
+        /// <summary>
+        /// Список сотрудников компании.
+        /// </summary>
+        public ICollection<EmployeeModel> Employees { get; private set; } = employees;
+
+        /// <summary>
+        /// Устанавливает список сотрудников компании и обновляет их принадлежность к текущей компании.
+        /// </summary>
+        /// <param name="employees">Список сотрудников компании.</param>
+        public void SetEmployees(ICollection<EmployeeModel> employees) =>
+            employees.ToList()
+                .ForEach(x => x.SetCompany(this));
+
+        /// <summary>
+        /// Обновляет данные компании на основе переданных параметров.
+        /// </summary>
+        /// <param name="name">Новое название компании.</param>
+        /// <param name="address">Новый адрес компании.</param>
+        /// <param name="industry">Новая отрасль компании.</param>
+        public void Update(
+            string? name,
+            string? address,
+            string? industry) 
+        {
+            if (name is not null)
+                Name = name;
+
+            if (address is not null)
+                Address = address;
+
+            if (industry is not null)
+                Industry = industry;
+        }
+    }
+}
---
+++ DB/Models/EmployeeModel.cs
@@ -0,0 +1,104 @@
+﻿namespace DB.Models
+{
+    /// <summary>
+    /// Модель сотрудника.
+    /// </summary>
+    public class EmployeeModel
+    {
+        /// <summary>
+        /// Уникальный идентификатор сотрудника.
+        /// </summary>
+        public Guid Id { get; private set; }
+
+        /// <summary>
+        /// Имя сотрудника.
+        /// </summary>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// Фамилия сотрудника.
+        /// </summary>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// Должность сотрудника.
+        /// </summary>
+        public string Position { get; private set; }
+
+        /// <summary>
+        /// Дата начала работы сотрудника в компании.
+        /// </summary>
+        public DateTime? EmploymentDate { get; private set; }
+
+        /// <summary>
+        /// Дата увольнения сотрудника.
+        /// </summary>
+        public DateTime? TerminationDate { get; private set; }
+
+        /// <summary>
+        /// Уникальный идентификатор компании, к которой принадлежит сотрудник.
+        /// </summary>
+        public Guid? CompanyId { get; private set; }
+
+        /// <summary>
+        /// Компания, к которой принадлежит сотрудник.
+        /// </summary>
+        public CompanyModel? Company { get; private set; }
+
+        public EmployeeModel(
+            string firstName,
+            string lastName,
+            string position,
+            CompanyModel company)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+
+            if (company is not null)
+                EmploymentDate = DateTime.UtcNow;
+
+            Company = company;
+        }
+
+        /// <summary>
+        /// Устанавливает компанию, к которой принадлежит сотрудник.
+        /// Если <paramref name="company"/> равно <see langword="null"/>, сотрудник увольняется из текущей компании.
+        /// </summary>
+        /// <param name="company">Компания, к которой принадлежит сотрудник.</param>
+        public void SetCompany(CompanyModel? company)
+        {
+            Company = company;
+            CompanyId = company?.Id;
+
+            if (company is null && Company is not null)
+                TerminationDate = DateTime.UtcNow;
+            else if (company is null)
+                EmploymentDate = null;
+            else
+                EmploymentDate = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Обновляет информацию о сотруднике на основе переданных параметров.
+        /// </summary>
+        /// <param name="firstName">Новое имя сотрудника.</param>
+        /// <param name="lastName">Новая фамилия сотрудника.</param>
+        /// <param name="position">Новая должность сотрудника.</param>
+        public void Update(
+            string? firstName,
+            string? lastName,
+            string? position)
+        {
+            if (firstName is not null)
+                FirstName = firstName;
+
+            if (lastName is not null)
+                LastName = lastName;
+
+            if (position is not null)
+                Position = position;
+        }
+    }
+}
