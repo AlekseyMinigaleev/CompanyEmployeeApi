7e4430cfde4f511f7d30d93d9629f868444cda2a
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-22 15:25:33 +0500

    Add dbContext

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<CompanyModel> Companies { get; set; }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
