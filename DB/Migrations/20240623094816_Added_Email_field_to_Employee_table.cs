ad0c78cff1e390ae4e9fb820e355f7c13e3c7843
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-23 14:49:02 +0500

    MIGRATION Added_Email_field_to_Employee_table

---
+++ DB/Migrations/20240623094816_Added_Email_field_to_Employee_table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Email_field_to_Employee_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employee",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employee");
+        }
+    }
+}
