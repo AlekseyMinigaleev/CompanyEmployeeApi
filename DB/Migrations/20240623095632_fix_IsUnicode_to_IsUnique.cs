568d7461f69f5ab49e6a129d4c82bfa5f62365fe
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-23 14:58:38 +0500

    MIGRATION fix IsUnicode to IsUnique and add unique index to Company.Name field

---
+++ DB/Migrations/20240623095632_fix_IsUnicode_to_IsUnique.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_IsUnicode_to_IsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_Email",
+                table: "Employee",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_Name",
+                table: "Company",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_Email",
+                table: "Employee");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Company_Name",
+                table: "Company");
+        }
+    }
+}
