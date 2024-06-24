73ef8ed79f9d7fa53a77ccaa6cdaa4cc98908b85
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-24 12:58:18 +0500

    MIGRATION fix relationship between tables

---
+++ DB/Migrations/20240624075515_fix_relationship_between_tables.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_relationship_between_tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Company_Id",
+                table: "Employee");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_CompanyId",
+                table: "Employee",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Company_CompanyId",
+                table: "Employee",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Company_CompanyId",
+                table: "Employee");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_CompanyId",
+                table: "Employee");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Company_Id",
+                table: "Employee",
+                column: "Id",
+                principalTable: "Company",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
