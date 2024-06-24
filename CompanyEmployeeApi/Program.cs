716f11da401ef635dd3b2a2e0c99db42f1207b53
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-24 19:39:17 +0500

    Configured Swagger using XML documentation

--- CompanyEmployeeApi/Program.cs
+++ CompanyEmployeeApi/Program.cs
@@ -2,9 +2,10 @@ using CompanyEmployeeApi.Extenstions;
 using CompanyEmployeeApi.Features.Company.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddControllersWithViews()
@@ -12,7 +13,15 @@ builder.Services.AddControllersWithViews()
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "CompanyEmployee API", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    options.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddStorage(builder.Configuration);
 builder.Services.AddServices();
 builder.Services.AddValidatorsFromAssemblyContaining<CompanyValidator>();
@@ -23,7 +32,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyEmployee API V1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
