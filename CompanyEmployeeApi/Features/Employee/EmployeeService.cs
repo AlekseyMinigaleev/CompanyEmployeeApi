cc07d6cbc42ceaf10f0944ec8b0e2adbac3218f1
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-24 14:03:47 +0500

    create company service

---
+++ CompanyEmployeeApi/Features/Employee/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using CompanyEmployeeApi.Features.Employee.Models;
+using DB;
+using DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployeeApi.Features.Employee
+{
+    public class EmployeeService(AppDbContext dbContext)
+    {
+        private readonly AppDbContext _dbContext = dbContext;
+
+        public async Task<EmployeeModel[]> GetAllAsync(CancellationToken cancellationToken) =>
+            await _dbContext.Employees
+                .Include(x => x.Company)
+                .ToArrayAsync(cancellationToken);
+
+        public async Task<EmployeeModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
+            await _dbContext.Employees
+                .Include(x => x.Company)
+                .SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+        public async Task<EmployeeModel> CreateAsync(
+            CreateEmployeeViewModel createEmployeeVM,
+            CancellationToken cancellationToken)
+        {
+            var company = await _dbContext.Companies
+                .SingleOrDefaultAsync(
+                    x => x.Id == createEmployeeVM.CompanyId,
+                    cancellationToken);
+
+            var employee = new EmployeeModel(
+                firstName: createEmployeeVM.FirstName,
+                lastName: createEmployeeVM.LastName,
+                position: createEmployeeVM.Position,
+                email: createEmployeeVM.Email,
+                company: company);
+
+            await _dbContext.Employees.AddAsync(employee, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return employee;
+        }
+    }
+}
